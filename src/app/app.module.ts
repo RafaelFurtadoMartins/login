0cee9e26ac331d31fae5538a2cd13d46ffe16576
RafaelFurtadoMartins <rafael.martins2231@gmail.com>
2020-11-18 13:37:35 -0300

    login, cadastro e home-page criadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastroComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
