d172c95e5919faf72c0ecad318c857def1b2ca67
RafaelFurtadoMartins <rafael.martins2231@gmail.com>
2020-11-18 14:01:37 -0300

    Rotas feitas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
+import { HomePageComponent } from './home-page/home-page.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {
+    path: '',
+    component: LoginComponent,
+    data: {
+      title: 'Login'
+    }
+  },
+  {
+    path: 'cadastro',
+    component: CadastroComponent,
+    data: {
+      title: 'Cadastro'
+    },
+  },
+  {
+    path: 'home-page',
+    component: HomePageComponent,
+    data: {
+      title: 'Home-Page'
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
