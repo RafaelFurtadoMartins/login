7d886fa09f0cfa086e64c5266f1052135a058d33
RafaelFurtadoMartins <rafael.martins2231@gmail.com>
2020-11-24 16:24:50 -0300

    Update cadastro.component.ts

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { ApiService } from 'src/service/api.service';
+import { GlobalService } from 'src/service/global.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -16,23 +19,47 @@ export class CadastroComponent implements OnInit {
   cpf: any;
   telefone: any;
   registrer: any;
-  formG
-  constructor() { }
+  formG: any;
+  loginData: any;
+  constructor(private apiService: ApiService, public globalService: GlobalService) { }
 
-  // ngOnInit(){
-  //   this.registrer = this.formG({
-  //     nome: ['', Validators.required],
-  //     email: ['', [Validators.required, Validators.email]],
-  //     cpf: [''],
-  //     cnpj: [''],
-  //     nomeEmpresa: [''],
-  //     senha: ['', Validators.required],
-  //     confirmarSenha: ['', Validators.required]
-  //   });
+  ngOnInit(){
+    this.registrer = this.formG({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      cpf: [''],
+      cnpj: [''],
+      nomeEmpresa: [''],
+      senha: ['', Validators.required],
+      confirmarSenha: ['', Validators.required]
+    });
 
-
-  // }
-  cadastro() {
 
   }
+  cadastro() {
+    this.apiService.cadastro({
+      nome: this.nome,
+      email: this.email,
+      cpf: this.cpf,
+      nomeEmpresa: this.nomeEmpresa,
+      cnpj: this.cnpj,
+      senha: this.senha,
+      confirmarSenha: this.confirmarSenha
+    })
+    .subscribe(data => {
+
+      if (data.success == true) {
+        this.loginData = data;
+      }
+      },
+      error => {
+
+        console.log('Erros: ', error);
+
+
+
+
+      }
+    );
 }
+};
