00eb0e077470b793c8cfc6f29ea2bee08d0a56f5
RafaelFurtadoMartins <rafael.martins2231@gmail.com>
2020-11-27 09:53:55 -0300

    1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,7 @@ import { GlobalService } from '../../service/global.service';
 import { Token } from '@angular/compiler';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/service/authentication.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -52,6 +53,10 @@ export class LoginComponent {
 
 
       );
+  }
+  
+  simpleAlert(){  
+    Swal.fire('Hello Angular');  
   }
 }
 
